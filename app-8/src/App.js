cd1d14a3e08562343b6dcbc884f73f2773b02a66
Roman <r0m4n.15@gmail.com>
2021-07-22 06:45:46 -0700

    final

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios'
+
+class App extends React.Component{
+  constructor(){
+    super();
+    
+    this.state= {
+      C3PO: ''
+    };
+  }
+
+  componentDidMount(){
+    axios.get('https://swapi.dev/api/people/2').then(res=>{
+      this.setState({
+        C3PO: res.data
+      });
+    });
+  }
+
+  render(){
+    return (
+      <div className ='App'>
+        <h1>name: {this.state.C3PO.name}</h1>
+        <h1>Birth Year: {this.state.C3PO.birth_year}</h1>
+        <h1>Height: {this.state.C3PO.height}</h1>
+        <h1>Eye Color: {this.state.C3PO.eye_color}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
